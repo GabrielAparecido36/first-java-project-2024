d212ed612ec57d72ef8b5f01729a2d00422afc2f
Gabriel Aparecido <contato.gabrielaparecido36@gmail.com>
2024-05-16 12:12:01 -0300

    correcoes code review

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Selecione o método: \n 1 - Hello World \n 2 - Calculadora básica \n 3 - Bhaskara"); // TODO Printar primeiro as opções e dps colocar o "Selecione o método:"
+        System.out.println("Selecione o método: \n 1 - Hello World \n 2 - Calculadora básica \n 3 - Bhaskara");
         int chamarMetodo = sc.nextInt();
 
-        switch (chamarMetodo) { // FIXME Definir default (pode printar algo no console, lançar exception, algo assim)
+        switch (chamarMetodo) {
             case 1:
                 helloWorld();
                 break;
@@ -17,6 +17,8 @@ public class Main {
             case 3:
                 bhaskara();
                 break;
+            default:
+                throw new RuntimeException("Valor inválido");
         }
     }
 
@@ -36,7 +38,7 @@ public class Main {
         System.out.println("Selecione a função matemática: \n 1 - Adição \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão");
         int operacaoMatematica = sc.nextInt();
 
-        switch (operacaoMatematica) { // FIXME Definir default (printar algo no console de opção inválida, lançar exception, algo assim)
+        switch (operacaoMatematica) {
             case 1:
                 System.out.print(num1 + num2);
                 break;
@@ -47,8 +49,13 @@ public class Main {
                 System.out.print(num1 * num2);
                 break;
             case 4:
-                System.out.print(num1 / num2); // FIXME Verificar se num2 != 0, caso contrário, tratar esse cenário (printar algo no console que não pode ser 0, lançar exception, algo assim)
+                if (num2 == 0) {
+                    throw new RuntimeException("Não é possível fazer divisão por 0");
+                }
+                System.out.print(num1 / num2);
                 break;
+            default:
+                throw new RuntimeException("Valor inválido");
         }
 
     }
@@ -68,13 +75,17 @@ public class Main {
         int delta = b * b - 4 * a * c;
         System.out.println("Valor de delta: " + delta);
 
-        double raizDelta = Math.sqrt(delta); // FIXME Adicionar tratamento para, quando delta < 0, retornar que "não existe raiz"
+        if (delta <= 0) {
+            throw new RuntimeException("Não existe raiz de número negativo");
+        }
+
+        double raizDelta = Math.sqrt(delta);
         System.out.println("Valor da raiz de delta: " + raizDelta);
 
-        double x1 = ((double) (-b) + raizDelta) / (double) (2 * a); // FIXME Dá pra remover esses cast pra double?
+        double x1 = ((-b) + raizDelta) / (2 * a);
         System.out.println("Valor de x¹: " + x1);
 
-        double x2 = ((double) (-b) - raizDelta) / (double) (2 * a); // FIXME Dá pra remover esses cast pra double?
+        double x2 = ((-b) - raizDelta) / (2 * a);
         System.out.println("Valor de x²: " + x2);
     }
 }
